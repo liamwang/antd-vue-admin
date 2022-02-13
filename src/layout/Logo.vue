848d87470cb4ddc9537d3624d83461c248650a5c
Liam <liam.wang@live.com>
2022-02-13 15:48:10 +0800

    fix: 点击 Logo 回到首页

--- src/layout/Logo.vue
+++ src/layout/Logo.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+  import { homePath } from '@/config/const'
+
   defineProps<{
     collapsed: boolean
   }>()
 
   const appName = import.meta.env.VITE_APP_NAME
+
+  const onClick = () => {
+    location.href = homePath
+  }
 </script>
 
 <template>
-  <div :class="['logo', collapsed ? 'collapsed' : '']" @click="() => (location.href = '/')">
+  <div :class="['logo', collapsed ? 'collapsed' : '']" @click="onClick">
     <img src="/logo.svg" alt="Logo" />
     <h1 v-if="!collapsed">{{ appName }}</h1>
   </div>
